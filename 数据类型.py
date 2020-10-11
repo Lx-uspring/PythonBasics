9f7c47947da2b94669711957c45cf2b55f1edccf
Lx-uspring <55077989+Lx-uspring@users.noreply.github.com>
2020-10-11 10:56:08 +0800

    数据类型和类型转换

---
+++ 数据类型.py
@@ -0,0 +1,29 @@
+# Name:LiuXing
+# DateTime:2020/10/11 0011 10:15
+# Details:
+
+#  整数默认整数是十进制
+n1=10
+n2=-10
+n3=0
+print(n1,type(n1))
+print(n2,type(n2))
+print(n3,type(n3))
+
+# 浮点数
+n4=1.1
+print(n4,type(n4))
+
+# 布尔
+n5=True
+print(n5,type(n5))
+
+# 字符串 '风火雷电' "风火雷电" '''风火雷电'''
+n6='风火雷电'
+n7="风火雷电"
+# 可以多行显示
+n8='''风火
+雷电'''
+
+print(n8)
+
---
+++ 类型转换.py
@@ -0,0 +1,16 @@
+# Name:LiuXing
+# DateTime:2020/10/11 0011 10:41
+# Details: 数据类型转换
+
+name='LiuXing'
+age=19
+# str() 将其他数据类型转换为字符串
+print('我叫'+name+',今年'+str(age))
+
+# int() 将其他数据类型转换为整数 限制：纯整数的字符串，浮点类型，布尔型
+
+# float()
+print(float('1.1'))
+print(float('1.1 '))
+print(float(False))
+print(float(12))
